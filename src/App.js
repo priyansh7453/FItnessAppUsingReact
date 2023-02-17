ebce8f5f8ad165f0d1d87151ecef1bbfdfb6e152
Priyansh kushwaha <priyanshkushwaha10@gmail.com>
2023-02-17 09:39:01 +0530

    images

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import logo from "./logo.svg";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { Box } from "@mui/material";
 
-function App() {
-  return <div className="App">vhg</div>;
-}
+const App = () => {
+  return (
+    <Box width="400px" sx={{ width: { x1: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/exercise/:id" element={ExerciseDetail}></Route>
+      </Routes>
+      <Footer />
+    </Box>
+  );
+};
 
 export default App;
